a150131de46b70cadc5890d47cc0c011d7c9d5da
Rogenes <rogenes.reis@gmail.com>
2017-08-31 21:22:41 -0300

    colisoes

--- core/src/br/cefetmg/games/collision/Collision.java
+++ core/src/br/cefetmg/games/collision/Collision.java
@@ -2,6 +2,7 @@ package br.cefetmg.games.collision;
 
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 /**
  * Utilitário para verificação de colisão.
@@ -17,8 +18,12 @@ public class Collision {
      * @return true se há colisão ou false, do contrário.
      */
     public static final boolean circlesOverlap(Circle c1, Circle c2) {
-        return false;
-    }
+        
+        Vector2 centro1 = new Vector2(c1.x,c1.y);
+        Vector2 centro2 = new Vector2(c2.x,c2.y);
+        
+        return (((c1.radius+c2.radius)*(c1.radius+c2.radius))>(centro1.dst2(centro2)));
+        }
 
     /**
      * Verifica se dois retângulos em 2D estão colidindo.
@@ -28,7 +33,42 @@ public class Collision {
      * @param r2 retângulo 2
      * @return true se há colisão ou false, do contrário.
      */
+   
     public static final boolean rectsOverlap(Rectangle r1, Rectangle r2) {
+        if(r2.x<r1.x+r1.width && r2.x+r2.width>r1.x){
+            if(r2.y<r1.y+r1.height && r2.y+r2.height>r1.y){
+                return true;
+            }
+        }
+        
         return false;
     }
+    
+    public static final boolean circlesVsRectangles(Rectangle r, Circle c){
+        
+        Vector2 c1 = new Vector2(c.x,c.y);
+        Vector2 r1 = new Vector2(r.x+r.width/2,r.y+r.height/2);
+        Vector2 distancia = new Vector2(c1.x-r1.x,c1.y-r1.y);
+        Vector2 projecaox = new Vector2(distancia.x,r1.y); 
+        Vector2 projecaoy = new Vector2(r1.x,distancia.y);
+        
+       
+        if(projecaox.len()>r.width/2){
+            projecaox.nor();
+            projecaox.scl(r.width/2);
+            
+        }
+        
+        if(projecaoy.len()>r.height/2){
+            projecaoy.nor();
+            projecaoy.scl(r.height/2);
+        }
+        
+        Vector2 p = new Vector2(projecaox.x,projecaoy.y); 
+      
+        return (c.radius >= p.dst(c1));
+        
+    
+    }
+    
 }
